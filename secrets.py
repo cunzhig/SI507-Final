cc460db498e0a6d949d81c8c920140c5a7fcfb30
cunzhig <42757911+cunzhig@users.noreply.github.com>
2018-12-09 23:46:39 -0500

    Delete secrets.py

--- secrets.py
+++
@@ -1,9 +0,0 @@
-#Yelp key
-API_KEY= '1224dRFC51oaFfOxrAekPIYFvTAh1Vf0dY_1uOtcHLtVPBqzL-h2pg-vDl421XcGHeUSmk8KGkNcSu4OnSs88ZbcZ2LypgEHKs82dcoPVgUO-auyUI-kaEMe4vDXW3Yx'
-# Plotly username and api_key here
-PLOTLY_USERNAME = "cunzhig"
-PLOTLY_API_KEY = "gXMlS06UgEBabQx7FQf5"
-
-# Don't change this
-MAPBOX_TOKEN = "pk.eyJ1IjoicHJpbmNpcGxleiIsImEiOiJjam1taTE3dGowamRjM3FqcG50MGp0anEwIn0.XuaFZy4Tff6aTfjiQUdd9Q"
-
